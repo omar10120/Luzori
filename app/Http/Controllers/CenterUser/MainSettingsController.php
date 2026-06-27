3f0bc4dc8f276bcb775b06097bb55756beff2338
tamer tamer <amr.dawoodi@hotmail.com>
2026-06-27 18:52:18 +0300

    update mainsettings controller

--- app/Http/Controllers/CenterUser/MainSettingsController.php
+++ app/Http/Controllers/CenterUser/MainSettingsController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers\CenterUser;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
-
+use Illuminate\Support\Str;
 
 class MainSettingsController extends Controller
 {
