dbb4dce03a83d6de884de7887b0cb9d6cbc364d0
tamer tamer <amr.dawoodi@hotmail.com>
2026-06-27 18:15:57 +0300

    update mainsettings controller

--- app/Http/Controllers/CenterUser/MainSettingsController.php
+++ app/Http/Controllers/CenterUser/MainSettingsController.php
@@ -5,15 +5,30 @@ namespace App\Http\Controllers\CenterUser;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
+
 class MainSettingsController extends Controller
 {
+    private CRUDService $crudService;
+    private $model = 'MainSetting';
+    private $plural = 'mainsettings';
+    private $indexRoute;
+    private $updateOrCreateRoute;
+    // public function __construct(CRUDService $crudService)
+    // {
+    //     $this->crudService = $crudService;
+    //     $this->indexRoute = 'center_user.' . $this->plural . '.index';
+    //     $this->updateOrCreateRoute = 'center_user.' . $this->plural . '.updateOrCreate';
+    // }
+    
     public function index(Request $request)
     {
-        $title = __('locale.settings') ?? 'Settings';
-        $menu = $title;
-        $menu_link = route('center_user.mainsettings.index');
-
-        $view = 'CenterUser.SubViews.mainSettings.index';
-        return view($view, compact('title', 'menu', 'menu_link'));
+        $can = 'VIEW_SETTINGS';
+        if (!auth('center_user')->user()->can($can, 'center_api')) {
+            return abort(403);
+        }
+        
+        $title = __('locale.settings');
+        $menu = __('locale.mainsettings');
+        return view("CenterUser.SubViews.mainSettings.index", compact('title', 'menu'));
     }
 }
