b124f28821e65ce4ba8399c8f1fff0a4aff1e649
tamer tamer <amr.dawoodi@hotmail.com>
2026-09-17 17:46:03 +0300

    update select center in set active

--- app/Http/Controllers/CenterUser/SalesController.php
+++ app/Http/Controllers/CenterUser/SalesController.php
@@ -688,7 +688,7 @@ class SalesController extends Controller
         $host = request()->getHost();
 
         if (in_array($host, ['127.0.0.1', 'localhost'], true)) {
-            return Center::with('media')->where('domain', 'center33')->first();
+            return Center::with('media')->where('domain', 'center')->first();
         }
 
         $domain = session('active_center_domain');
